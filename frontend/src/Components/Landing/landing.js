2ebb38a1edf9034dfaf18fc99ac291c3bfe79fb7
TylerRyan16 <92388210+TylerRyan16@users.noreply.github.com>
2025-01-31 15:09:31 -0500

    landing updated

--- frontend/src/Components/Landing/landing.js
+++ frontend/src/Components/Landing/landing.js
@@ -4,10 +4,31 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
     return (
-        <div className="landing-container">
-            <h1>LANDING PAGE</h1>
-            <Link to="/login" className="login-button">Login</Link>
-            <Link to="/create-profile" className="create-profile-button">Create Profile</Link>
+        <div className="landing-page-container">
+            <div className="centered-container">
+                <div className="items">
+                    <h1>LANDING PAGE</h1>
+                    <label htmlFor="email">Email </label>
+                    <input
+                        className="login-input"
+                        type="text"
+                        name="email"
+                        placeholder="Email..."
+                    />
+                    <label htmlFor="password">Email </label>
+                    <input
+                        className="login-input"
+                        type="password"
+                        name="password"
+                        placeholder="Password..."
+                    />
+                    <Link to="/login" className="login-button">Login</Link>
+                    <span className="or">Or</span>
+                    <Link to="/create-profile" className="create-profile-button">Create Profile</Link>
+                </div>
+
+            </div>
+
         </div>
     );
 
