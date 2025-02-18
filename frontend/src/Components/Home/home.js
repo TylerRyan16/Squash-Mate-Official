2157fc92d6caa1add30c2d05eb3c03b19fbfaf9e
Julianne <juk99@pitt.edu>
2025-02-18 00:20:58 -0500

    fixed button bug

--- frontend/src/Components/Home/home.js
+++ frontend/src/Components/Home/home.js
@@ -4,10 +4,24 @@ import { useEffect, useState } from 'react';
 
 
 // MAIN EXPORT
+function scrollLeft(id) {
+    const itemWidth = 150
+    const padding = 10
+    const pubGamesList = document.getElementById(id)
+    pubGamesList.scrollLeft -= itemWidth + padding
+  }
+
+function scrollRight(id) {
+    const itemWidth = 150
+    const padding = 10
+    const pubGamesList = document.getElementById(id)
+    pubGamesList.scrollLeft += itemWidth + padding
+  }
+
 const Home = () => {
     const navigate = useNavigate();
 
-    const videos = ['https://img.youtube.com/vi/23urWKmHS6o/0.jpg','https://img.youtube.com/vi/BxxfExJR63g/0.jpg','https://img.youtube.com/vi/6lVrj_XokGo/0.jpg', 'https://img.youtube.com/vi/23urWKmHS6o/0.jpg','https://img.youtube.com/vi/BxxfExJR63g/0.jpg','https://img.youtube.com/vi/6lVrj_XokGo/0.jpg','https://img.youtube.com/vi/23urWKmHS6o/0.jpg','https://img.youtube.com/vi/BxxfExJR63g/0.jpg','https://img.youtube.com/vi/6lVrj_XokGo/0.jpg','https://img.youtube.com/vi/23urWKmHS6o/0.jpg','https://img.youtube.com/vi/BxxfExJR63g/0.jpg','https://img.youtube.com/vi/6lVrj_XokGo/0.jpg','https://img.youtube.com/vi/23urWKmHS6o/0.jpg','https://img.youtube.com/vi/BxxfExJR63g/0.jpg','https://img.youtube.com/vi/6lVrj_XokGo/0.jpg']
+    const videos = ['https://img.youtube.com/vi/n05wyIGkeIA/0.jpg','https://img.youtube.com/vi/Lbdp-HCjWw8/0.jpg','https://img.youtube.com/vi/5Dd_LS5mWUE/0.jpg', 'https://img.youtube.com/vi/n05wyIGkeIA/0.jpg','https://img.youtube.com/vi/Lbdp-HCjWw8/0.jpg','https://img.youtube.com/vi/5Dd_LS5mWUE/0.jpg','https://img.youtube.com/vi/n05wyIGkeIA/0.jpg','https://img.youtube.com/vi/Lbdp-HCjWw8/0.jpg','https://img.youtube.com/vi/5Dd_LS5mWUE/0.jpg',]
     const isLoggedIn = () => {
         const loggedIn = localStorage.getItem('authToken');
         console.log("logged in: ", loggedIn);
@@ -26,43 +40,16 @@ const Home = () => {
     const itemWidth = 150
     const padding = 10
 
-    const myVideosPrev = document.getElementById('left-scroll-my-videos')
-    const myVideosNext = document.getElementById('right-scroll-my-videos')
-    console.log(myVideosNext)
-    const myVideosList = document.getElementById('my-videos-list')
-    console.log(myVideosList)
-    myVideosPrev.addEventListener('click',()=>{
-            myVideosList.scrollLeft -= itemWidth + padding
-        })
-    myVideosNext.addEventListener('click',()=>{
-            myVideosList.scrollLeft += itemWidth + padding
-          })
-    const pubGamesPrev = document.getElementById('left-scroll-public')
-    const pubGamesNext = document.getElementById('right-scroll-public')
-    const pubGamesList = document.getElementById('public-games-list')
-    pubGamesPrev?.addEventListener('click',()=>{
-            pubGamesList.scrollLeft -= itemWidth + padding
-        })
-    pubGamesNext?.addEventListener('click',()=>{
-            pubGamesList.scrollLeft += itemWidth + padding
-          })
-    const sharedPrev = document.getElementById('left-scroll-shared')
-    const sharedNext = document.getElementById('right-scroll-shared')
-    const sharedList = document.getElementById('shared-list')
-    sharedPrev.addEventListener('click',()=>{
-            sharedList.scrollLeft -= itemWidth + padding
-        })
-    sharedNext.addEventListener('click',()=>{
-            sharedList.scrollLeft += itemWidth + padding
-          })  
+
+    
     return (
         <div className="page-container">
-            <h1 class='top-header'>Squash Mate add logo?</h1>
+            <h1 class='top-header'>Squash Mate</h1>
             <h2 class = 'top-header'>Elevate Your Game</h2>
             <h1 class = 'category-name'>My Videos</h1>
             <div class="scroll-container">
                 <div class="carousel">
-                    <button id="left-scroll-my-videos" class="left-scroll">
+                    <button id="left-scroll-my-videos" class="left-scroll" onClick = {() => scrollLeft('my-videos-list')}>
                         <img class= 'left-scroll-icon' id='left-scroll-icon' src='assets\icons\right-arrow.png' alt=''/>
                     </button>
                     <div id="my-videos-list" class="my-videos-list">
@@ -77,7 +64,7 @@ const Home = () => {
                         ))}
 
                     </div>
-                    <button id="right-scroll-my-videos" class="right-scroll">
+                    <button id="right-scroll-my-videos" class="right-scroll" onClick={() =>scrollRight("my-videos-list")}>
                         <img class='right-scroll-icon' id='right-scroll-icon' src='assets\icons\right-arrow.png' alt=''/>
                     </button>
                 </div>
@@ -85,7 +72,7 @@ const Home = () => {
             <h1 class='category-name'>Public Games</h1>
             <div class="scroll-container">
                 <div class="carousel">
-                    <button id="left-scroll-public" class="left-scroll">
+                    <button id="left-scroll-public" class="left-scroll" onClick = {() => scrollLeft('public-games-list')}>
                         <img class= 'left-scroll-icon' id='left-scroll-icon' src='assets\icons\right-arrow.png' alt=''/>
                     </button>
                     <div id="public-games-list" class="my-videos-list">
@@ -99,15 +86,15 @@ const Home = () => {
                         </div>
                         ))}
                     </div>
-                    <button id="right-scroll-public" class="right-scroll">
-                        <img class='right-scroll-icon' id='right-scroll-icon' src='assets\icons\right-arrow.png' alt=''/>
+                    <button id="right-scroll-public" className="right-scroll" onClick={() =>scrollRight("public-games-list")}>
+                        <img className='right-scroll-icon' id='right-scroll-icon' src='assets\icons\right-arrow.png' alt=''/>
                     </button>
                 </div>
             </div>
             <h1 class='category-name'>Shared With Me</h1>
             <div class="scroll-container">
                 <div class="carousel">
-                    <button id="left-scroll-shared" class="left-scroll">
+                    <button id="left-scroll-shared" class="left-scroll" onClick = {() => scrollLeft('shared-list')}>
                         <img class= 'left-scroll-icon' id='left-scroll-icon' src='assets\icons\right-arrow.png' alt=''/>
                     </button>
                     <div id="shared-list" class="my-videos-list">
@@ -121,7 +108,7 @@ const Home = () => {
                         </div>
                         ))}
                     </div>
-                    <button id="right-scroll-shared" class="right-scroll">
+                    <button id="right-scroll-shared" class="right-scroll" onClick={() =>scrollRight("shared-list")}>
                         <img class='right-scroll-icon' id='right-scroll-icon' src='assets\icons\right-arrow.png' alt=''/>
                     </button>
                 </div>
