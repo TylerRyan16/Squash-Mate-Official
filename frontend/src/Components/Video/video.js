b7c9cf33672e7b94a0a86b4ef56212e6b06bf25a
Julianne <juk99@pitt.edu>
2025-02-20 18:09:41 -0500

    starting comment section

--- frontend/src/Components/Video/video.js
+++ frontend/src/Components/Video/video.js
@@ -1,6 +1,29 @@
 import './video.scss';
 import { useRef, useState } from 'react';
 
+function openCoach(evt, coachName){
+
+    console.log("We are here")
+    var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(coachName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+}
+
 const Video = () => {
     const playerRef = useRef(null);
     const [player, setPlayer] = useState(null);
@@ -20,10 +43,36 @@ const Video = () => {
                 <h1>Video Player.</h1>
 
                 {/* Video Element */}
-                <video ref={playerRef} width="720" controls>
-                    <source src="" type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
+                <div className = 'horizontal-flex'>
+                    <video ref={playerRef} width="720" controls>
+                        <source src="" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    <div className='comment-section'>
+                        <div class="coach-tabs">
+                            <button class="tablinks" onClick={(event) => openCoach(event, '1')}>Coach #1</button>
+                            <button class="tablinks" onClick={(event) => openCoach(event, '2')}>Coach #2</button>
+                            <button class="tablinks" onClick={(event) => openCoach(event, '3')}>Coach #3</button>
+                        </div>
+                        <div id="1" class="tabcontent">
+                            <h3>Coach #1 Stuff</h3>
+                            <p>London is the capital city of England.</p>
+                        </div>
+
+                        <div id="2" class="tabcontent">
+                            <h3>Coach #2 Stuff</h3>
+                            <p>Paris is the capital of France.</p>
+                        </div>
+
+                        <div id="3" class="tabcontent">
+                            <h3>Coach #3</h3>
+                            <p>Tok Stuffyo is the capital of Japan.</p>
+                        </div>
+
+
+                    </div>
+                </div>
+                
 
                 <div className="controls">
                     <button onClick={togglePlay}></button>
