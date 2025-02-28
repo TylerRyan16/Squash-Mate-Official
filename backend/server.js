547017e147ea5d277ca862f49a0efddbadbcd661
TylerRyan16 <92388210+TylerRyan16@users.noreply.github.com>
2025-02-28 10:27:11 -0500

    test if my esrver changes broke vercel?

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use(cookieParser());
 // middlewar
 app.use(cors({
     origin: ["https://squash-mates.onrender.com", "http://localhost:3000", "https://squashmate.vercel.app", "https://squashmate.club", "https://www.squashmate.club"],
-    credentials: true
 }));
 app.use(bodyParser.json());
 
@@ -27,5 +26,5 @@ app.use("/api/comments", commentsRoute);
 // start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on https://squash-mates.onrender.com:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
